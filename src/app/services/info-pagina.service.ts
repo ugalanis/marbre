20211ae7e681eade46723c2b5fd61870a79cddfe
Uriel Galanis <uriel.galanis@gmail.com>
2020-06-19 16:06:52 -0300

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,24 +9,33 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   cargada: boolean = false;
-  info: InfoPagina = {};
+  info: InfoPagina = {};  
+  equipo: any[] = [];
 
-  constructor( private http: HttpClient) { 
-
+  constructor (private http: HttpClient) { 
     //console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     //Leo archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-        
+        .subscribe( (resp: InfoPagina) => {        
           this.cargada = true;
           this.info = resp;
           //(resp: any) es otra opción para que tome las prioridades como un objeto
-          // console.log(resp); console.log( resp['email'] );
-          console.log(resp);
-        
+          // console.log(resp); console.log( resp['email'] );                
         });
+  }
 
+  private cargarEquipo() {
+    //Leo archivo JSON
+    this.http.get('https://angular-html-a5c98.firebaseio.com/equipo.json')    
+        .subscribe( (resp: any[]) => {
+          this.equipo = resp;
+          console.log(resp);
+        });
   }
 
 }
